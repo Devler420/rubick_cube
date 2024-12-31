6d7d9b149e6ebb96bbd590996a454170f81b74dc
Boom P <boompanupong@gmail.com>
2024-12-31 12:52:34 +0700

    generate random choices

--- main.py
+++ main.py
@@ -57,7 +57,25 @@ def remove_piece(cube, num_pieces):
             cube[x][y][z] = ' '
             i += 1
 
+def generate_choices(cube, removed_blocks):
+    choices = []  # List to store all choices
 
+    correct_choice = removed_blocks[:] # copy of the list of removed blocks
+    choices.append(correct_choice)
+
+    while len(choices) < 4:
+        distractor = []
+        for _ in range(len(removed_blocks)):
+            random_pos = [random.randrange(0, 3), random.randrange(0, 3), random.randrange(0, 3)]
+
+            if random_pos not in distractor and (random_pos) not in correct_choice:
+                distractor.append(random_pos)
+
+        if distractor not in choices:
+            choices.append(distractor)
+
+    random.shuffle(choices)
+    return choices
 
 cube = generate_cube()
 removed_block_list = [];
@@ -68,3 +86,7 @@ print('removed_block_list: ')
 print(removed_block_list)
 print()
 print_pretty_cube(cube)
+print('\nMultiple Choices: ')
+final_choices = generate_choices(cube, removed_block_list)
+for i, choice in enumerate(final_choices):
+    print(f"Option {i + 1}: {choice}")
